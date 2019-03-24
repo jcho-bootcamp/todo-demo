13a2c9e537916c4ab438c445080b22284ec4a861
June Hyok Cho <jcho0627@gmail.com>
2019-03-24 16:44:07 +0100

    todo demo complete

---
+++ app.js
@@ -0,0 +1,20 @@
+const lis = document.querySelectorAll("li");
+
+// Not recommended to change to arrow function (if necessary "this" needs to change as well)
+// https://dmitripavlutin.com/when-not-to-use-arrow-functions-in-javascript/
+
+for (let i = 0; i < lis.length; i++) {
+  // Add class green while mouse
+  lis[i].addEventListener("mousemove", function () {
+    this.classList.add("green");
+  });
+  // Remove class green while no mouse
+  lis[i].addEventListener("mouseout", function () {
+    this.classList.remove("green");
+  });
+
+  // Add class line-through after click
+  lis[i].addEventListener("click", function () {
+    this.classList.toggle("line-through");
+  });
+}
